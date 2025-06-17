5b596b65c5c349f74eaf29c7355669e5aa1742bb
Artur <metallist15431543@gmail.com>
2025-06-17 20:57:30 +0300

    change files structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./store/store.ts";
+import { store } from "./api/store.ts";
 
 import App from "./App.tsx";
 
